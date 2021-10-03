2fd7d559b570ffca66f60f31aef9ae3698bc208f
sumancrest0001 <suman.crest0001@gmail.com>
2021-10-03 13:41:54 +0545

    Now approved post can't be updated by the creator

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -24,8 +24,19 @@ describe 'navigate' do
       user = FactoryBot.create(:user)
       login_as(user, :scope => :user)
 
-      visit edit_post_path(@post)
+      visit edit_post_path(@post) 
       expect(page).to_not have_content('Approved')
+    end
+
+    it 'should not be editable by the post creator if status is approved' do
+      logout(:user)
+      user = FactoryBot.create(:user)
+      login_as(user, :scope => :user)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      visit edit_post_path(@post)
+      expect(current_path).to eq(root_path)
     end
   end
 end
