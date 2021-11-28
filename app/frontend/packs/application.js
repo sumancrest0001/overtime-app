3133627c93f9a0ff7c6c86907d9c28b77edeb89a
sumancrest0001 <suman.crest0001@gmail.com>
2021-11-28 10:27:43 +0545

    Complete interfaces

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,8 +8,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("gritter/js/jquery.gritter.js")
-import "bootstrap"
-import "@fortawesome/fontawesome-free/css/all"
+import '../js/bootstrap_js_files.js'
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
